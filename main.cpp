8f49126dc6ef513e31712fb834764bb228a03f2e
smallsanalytics <elizabeth@smallsanalytics.com>
2018-04-12 03:06:38 +0300

    Added duel_result hook

--- main.cpp
+++ main.cpp
@@ -38,16 +38,24 @@ void OnDuelStart(void *_this, XID *target)
 TrampolineHook<void(void*,XID*,bool,char)> *DiePatcher;
 void Die(void *_this, XID *attacker, bool is_pariah, char attack_mode)
 {
-    printf("[PLUGIN] Die: received values attacker.id = %d, attacker.type = %d, is_pariah = %b, attack_mode = %c", attacker->id, attacker->type, is_pariah, attack_mode);
+    printf("[PLUGIN] Die: received values attacker.id = %d, attacker.type = %d, is_pariah = %d, attack_mode = %c\n", attacker->id, attacker->type, is_pariah, attack_mode);
 
     DiePatcher->CallOriginal(_this, attacker, is_pariah, attack_mode);
 }
 
 
+TrampolineHook<void(void*, XID*, bool)> *duel_resultParcher;
+void duel_result(void *_this, XID* target, bool is_failed)
+{
+    printf("[PLUGIN] duel_result: received values target.id = %d, target.type = %d, is_failed = %d\n", target->id, target->type, is_failed);
+    duel_resultParcher->CallOriginal(_this, target, is_failed);
+}
+
 void module_load(void)
 {
 	printf("[PLUGIN] GainMoney: loaded\n");
 	DiePatcher = new TrampolineHook<void(void*, XID*, bool, char)>(0x0807D78E, Die);
+	duel_resultParcher = new TrampolineHook<void(void*, XID*, bool)>(0x080708A0, duel_result);
     OnDuelStartPatcher = new TrampolineHook<void(void*, XID*)>(0x0811D08A, OnDuelStart);
 	GainMoneyWithDropPatcher = new TrampolineHook<void(void*, size_t, bool)>(0x0807CEC8, GainMoneyWithDrop);
     printf("[PLUGIN] GainMoney: patched\n");
